1197cf07d44478fbc234afd0a62b4fc110d9313e
Andrew Accuardi and Tyler Ferrin <andrew@>
2016-12-01 09:42:15 -0800

    Add passing spec to return the city value.

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -9,4 +9,11 @@ describe(Address) do
       expect(test_address.street()).to(eq("1234 SE Main St."))
     end
   end
+
+  describe("#city") do
+    it("Will return the name of the city.") do
+      test_address = Address.new(:street => "1234 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214")
+      expect(test_address.city()).to(eq("Portland"))
+    end
+  end
 end
