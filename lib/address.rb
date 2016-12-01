02cfe8b2be7637d10d7f4a9384455b2cf00d78af
Andrew Accuardi and Tyler Ferrin <tyler@>
2016-12-01 10:25:08 -0800

    Creates passing methods/specs for Address class.

--- lib/address.rb
+++ lib/address.rb
@@ -1,10 +1,39 @@
 class Address
-  attr_reader(:street, :city, :state, :zip)
+  @@addresses = []
+  attr_reader(:street, :city, :state, :zip, :type)
 
   def initialize(attributes)
     @street = attributes.fetch(:street)
     @city = attributes.fetch(:city)
     @state = attributes.fetch(:state)
     @zip = attributes.fetch(:zip)
+    @type = attributes.fetch(:type)
+    @id = @@addresses.length() + 1
+  end
+
+  def id
+    @id
+  end
+
+  def save
+    @@addresses.push(self)
+  end
+
+  def self.all
+    @@addresses
+  end
+
+  def self.clear
+    @@addresses = []
+  end
+
+  def self.find(id)
+    found_address = nil
+    @@addresses.each do |address|
+      if address.id().eql?(id)
+        found_address = address
+      end
+    end
+    found_address
   end
 end
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -2,25 +2,72 @@ require("rspec")
 require("address")
 
 describe(Address) do
+  before() do
+    @test_address = Address.new(:street => "1234 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214", :type => "home")
+    @test_address2 = Address.new(:street => "1235 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214", :type => "home")
+    Address.clear()
+  end
 
   describe("#street") do
     it("Will return the name of the street including house number.") do
-      test_address = Address.new(:street => "1234 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214")
-      expect(test_address.street()).to(eq("1234 SE Main St."))
+      expect(@test_address.street()).to(eq("1234 SE Main St."))
     end
   end
 
   describe("#city") do
     it("Will return the name of the city.") do
-      test_address = Address.new(:street => "1234 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214")
-      expect(test_address.city()).to(eq("Portland"))
+      expect(@test_address.city()).to(eq("Portland"))
     end
   end
 
   describe("#state") do
     it("Will return the name of the state.") do
-      test_address = Address.new(:street => "1234 SE Main St.", :city => "Portland", :state => "OR", :zip => "97214")
-      expect(test_address.state()).to(eq("OR"))
+      expect(@test_address.state()).to(eq("OR"))
+    end
+  end
+
+  describe("#zip") do
+    it("Will return the name of the zip.") do
+      expect(@test_address.zip()).to(eq("97214"))
+    end
+  end
+
+  describe("#type") do
+    it("Will return the name of the type.") do
+      expect(@test_address.type()).to(eq("home"))
+    end
+  end
+
+  describe("#id") do
+    it("Will return the id of the address.") do
+      expect(@test_address.id()).to(eq(1))
+    end
+  end
+
+  describe(".all") do
+    it("Will return the class array.") do
+      expect(Address.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("Will clear the class array.") do
+      expect(Address.clear()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("Will save the current address.") do
+      @test_address.save()
+      expect(Address.all()).to(eq([@test_address]))
+    end
+  end
+
+  describe(".find") do
+    it("Will find a specific address in the address array.") do
+      @test_address.save()
+      @test_address2.save()
+      expect(Address.find(2)).to(eq(@test_address2))
     end
   end
 end
